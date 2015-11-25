d0cfb98fbf6ffb65dc2a15e6f4691fd474a5454c
rppksdei <raman.sdei@gmail.com>
2015-11-25 15:04:53 +0530

    patient login

--- routes/front_patient.js
+++ routes/front_patient.js
@@ -9,8 +9,9 @@ module.exports = function(app, express){
         res.send("sdfdsf");
         //patientFrontObj.getPatientDetail(req, res);
     });*/
+
     function supportCrossOriginScript(req, res, next) {
-        console.log('RAMAN',req.method);
+    
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -19,12 +20,9 @@ module.exports = function(app, express){
     }
 
     router.post('/login', supportCrossOriginScript, function(req, res){
-        console.log('==========================================================================================');
-        console.log('here with post');
-        console.log(req.body);
-        //patientFrontObj.login(req, res);
-        console.log(patientFrontObj);
-        patientFrontObj.login(req, res, next);
+        patientFrontObj.login(req, res, function(err, data){
+        console.log("test");
+    });
     });
     app.use('/front_patient',router);
 }
