5098254987f80501eec4c1eb55c32d479c049df1
gurpreets89 <gurpreettest2014@gmail.com>
2015-12-11 16:44:50 +0530

    master branch node module pending commits. Irrelevant.

--- routes/cron.js
+++ routes/cron.js
@@ -3,23 +3,12 @@ module.exports = function(app,express){
     var cronController = require("./../controller/cronController");
     var cronObj = new cronController();
     
-    //router.get('/detail', function(req, res, next){
-    /*setInterval(
-    	router.get('/detail', function(req, res){
-    		console.log('Ramanpreet');
-        	cronObj.getlisting(req, res);
-    	}), 6000);*/
+    /* router.get('/detail', function(req, res, next){ */
+    
     setInterval(function(){ 
-		//req = {};res = {};
-		console.log('Cron Job');
-		cronObj.getlisting();
-    },600000);
-
-    /*router.get('/detail', function(req, res){
-		console.log('Ramanpreet');
-    	cronObj.getlisting(req, res);
-    	// cronObj.amit(req, res, next);
-	})*/
+	console.log('Cron Job');
+	cronObj.getlisting();
+    },600000); // 5 0s
 
     app.use('/cron',router);
 }
