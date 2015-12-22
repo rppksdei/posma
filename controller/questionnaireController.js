0216bf3e3c1c9d20992d8e41254af7155219507a
rppksdei <raman.sdei@gmail.com>
2015-12-22 12:26:31 +0530

    questionnaire controller

--- controller/questionnaireController.js
+++ controller/questionnaireController.js
@@ -141,6 +141,7 @@ updateQuestionnaireDetail = function(req, res){
     //End of code to update clinic detail
 }
 
+
 module.exports = function(){
     this.getlisting = getlisting;
     this.getQuestionnaireDetail = getQuestionnaireDetail;
