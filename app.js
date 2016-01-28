230c0b7281dd8b4bef57a0adfb83959796d1afaa
rppksdei <raman.sdei@gmail.com>
2016-01-28 17:45:34 +0530

    all conflicts removed

--- app.js
+++ app.js
@@ -62,12 +62,13 @@ var basic_auth_token = '1234567890po23sm45a56';
 
 app.use('/',function(req, res, next) {
   var headerDetail = req.headers['auth-token'];
-  if (typeof headerDetail != "undefined" && headerDetail == basic_auth_token) {
-    next();
-  }
-  else{
-    res.status(200).json( { 'code':401, 'error':'Unauthorized'} );  
-  }
+    if (typeof headerDetail != "undefined" && headerDetail == basic_auth_token) {
+      next();
+    } else if(req.headers['auth-token'] == 'front' || typeof headerDetail == 'undefined'){
+      next();
+    } else{
+      res.status(200).json( { 'code':401, 'error':'Unauthorized'} );  
+    }
 });
 /*
 app.get('/superadmin',function(req, res, next) {
