96457800d55620cb57c88c3f44acbc8e2d618bcd
rppksdei <raman.sdei@gmail.com>
2016-02-29 11:21:00 +0530

    reports changes

--- controller/reportController.js
+++ controller/reportController.js
@@ -5,7 +5,7 @@ getlisting = function(req, res, next){
     var search = {};
     if(typeof req.body.limit != undefined){
     	var recLimit = {'limit':req.body.limit};
-		patientAnswerModel.getList(search, {created:-1}, ,function(err, answerDetails){
+		patientAnswerModel.getList(search, {created:-1}, function(err, answerDetails){
 	        if(err){
 	            res.json(err);
 	        } else{
